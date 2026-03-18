009b56904091b6c1236e492854647ff11f176fbb
Tetiana <tanya67722@gmail.com>
2026-03-18 20:27:32 +0200

    setup project structure, styles, routing and layout

---
+++ src/app/routes.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from "../components/layout/Layout.tsx";
+import Home from "../pages/Home/Home.tsx";
+
+
+export const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route element={<Layout />}>
+                <Route path="/" element={<Home />} />
+            </Route>
+        </Routes>
+    );
+};
