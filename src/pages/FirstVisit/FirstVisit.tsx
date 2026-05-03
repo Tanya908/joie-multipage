1752bd50cddc27c5a5f42c8172f485eff923ec28
Tetiana <tanya67722@gmail.com>
2026-05-03 16:21:21 +0300

    implement start section on firstVisit page

--- src/pages/FirstVisit/FirstVisit.tsx
+++ src/pages/FirstVisit/FirstVisit.tsx
@@ -1,10 +1,13 @@
 import Hero from "./Hero.tsx";
 import FindUs from "../../components/map/FindUs.tsx";
 import mapBackgroundBlue from "../../assets/map/mapBackgroundBlue.svg";
+import Start from "./Start.tsx";
+
 const FirstVisit = () => {
     return (
         <>
             <Hero/>
+            <Start/>
             <FindUs background={mapBackgroundBlue} />
         </>
     )
---
+++ src/pages/FirstVisit/Start.tsx
@@ -0,0 +1,69 @@
+import line from "../../assets/first-visit/start/line.svg";
+import childrenFirst from "../../assets/first-visit/start/childrenFirst.webp";
+import childrenSecond from "../../assets/first-visit/start/childrenSecond.webp";
+
+type StartProps = {
+    image: string;
+    title: string;
+    description: string;
+    id: number;
+}
+
+const StartItems:StartProps[] = [
+    {
+        image:childrenFirst,
+        title:"More than a checkup",
+        description:"Whether your child is an infant, toddler, or school-aged, this first appointment is about more than just checking teeth. It’s about building trust, setting a strong foundation, and making both kids and parents feel comfortable every step of the way.",
+        id:1
+    },
+    {
+        image:childrenSecond,
+        title:"A gentle start to lifelong smiles",
+        description:"At Joie Pediatric Dentistry, your child’s first visit sets the tone for how they’ll feel about dental care for years to come – so we make it calm, positive and even a little fun.",
+        id:2
+    },
+]
+
+const Start = () => {
+    return (
+        <section className="mt-24 px-4 md:px-8 grid grid-cols-1 md:grid-cols-2 items-center md:items-start mx-auto
+                            justify-items-center md:justify-items-start gap-8 md:gap-10 max-w-[1600px]"
+        >
+            <h1 className="text-h1 mb-12 md:mb-0 order-1 md:order-2 md:self-start">
+                A gentle {" "}
+                <span className="relative inline-block">
+                    start {" "}
+                        <img src={line}
+                             alt=""
+                             aria-hidden="true"
+                             className="absolute left-0 -bottom-2 pointer-events-none scale-110"
+                        />
+                </span>
+                {" "} to lifelong healthy habits
+            </h1>
+
+
+                {StartItems.map((item, index) => (
+                    <div
+                        key={item.id}
+                        className={`flex flex-col max-w-xl xl:max-w-2xl  
+                                    ${index === 0 ? "order-2 md:order-1 md:row-span-2" 
+                                                  : "order-3 md:order-3"}`}
+                    >
+                        <img
+                            src={item.image}
+                            className="w-full rounded-4xl mb-8"
+                            alt={item.title}
+                        />
+                        <h4 className="text-h4-dec mb-4">
+                            {item.title}
+                        </h4>
+                        <p className="text-p1 text-[var(--color-black)]">
+                            {item.description}
+                        </p>
+                    </div>
+                ))}
+        </section>
+    )
+}
+export default Start
